3aec65b26d1d135b3254aec6034863cfb6fd94d9
Kevin Herrera <1herrerake@gmail.com>
2016-12-12 12:20:42 -0500

    react password validator

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+class User extends Component {
+  constructor (props) {
+      super(props)
+      this.state = {
+        username: '',
+        password: '',
+        validation: '',
+        valid: true
+      }
+    }
 
-export default App;
+    username (e) {
+      this.setState({username: e.target.value})
+    }
+
+    password (e) {
+      this.setState({password: e.target.value})
+    }
+
+    validation (e) {
+      this.setState({validation: e.target.value})
+    }
+
+    submit (e) {
+      e.preventDefault()
+      this.checkValidity()
+    }
+
+    checkValidity () {
+      if (this.state.password === this.state.passwordConfirm) {
+          this.setState({ valid: true })
+      } else {
+        this.setState({ valid: false })
+      }
+    }
+
+    render () {
+      let validText = this.state.valid ? "Valid!" : "Passwords do not match"
+      return (
+          <div className="User">
+            <input onChange={(e) => this.username(e)} type="text" placeholder="username" />
+            <br></br>
+            <input onChange={(e) => this.password(e)} type="password" placeholder="password" />
+            <br></br>
+            <input onChange={(e) => this.validation(e)} type="password" placeholder="confirm password" />
+            <br></br>
+            <input type="submit" onClick={(e) => this.submit(e)} value="submit" />
+            <p>{validText}</p>
+          </div>
+      );
+    }
+  }
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import User from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <User />,
   document.getElementById('root')
 );
